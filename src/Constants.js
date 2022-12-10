9a4448eaa9ef4d5b491cdd8470b018c4ca9ae6f9
rasedul99 <rasedul2199@gmail.com>
2022-12-10 12:49:15 +0600

    portfolio

---
+++ src/Constants.js
@@ -0,0 +1,41 @@
+import cryptoCafe from "../src/Assets/Projects/cryptocafe.png";
+import tastytreat from "../src/Assets/Projects/tatytreat.png";
+
+export const PROJECTS = [
+  {
+    image: tastytreat,
+    name: "A Food Order website",
+    technologyUsed: "ReactJs, Javascript, Redux,Tailwind CSS",
+    description: "Tasty Treat is where user can order their foods.",
+    url: "https://tastyy-foods.netlify.app/",
+  },
+  {
+    image: cryptoCafe,
+    name: "Crypto Cafe",
+    technologyUsed: " React,Bootstrap,React Bootstrap",
+    description:
+      "A Web application that displays coin information from Coingechko API",
+    url: "https://travlwell.com/",
+  },
+];
+
+export const SKILLS = [
+  { name: "ReactJs", initialRating: 4 },
+  { name: "JavaScript", initialRating: 4 },
+  { name: "Redux", initialRating: 4 },
+  { name: "Nodejs", initialRating: 3 },
+  { name: "Html", initialRating: 4 },
+  { name: "CSS", initialRating: 4 },
+  { name: "TypeScript", initialRating: 3 },
+  { name: "Bootstrap | Tailwind CSS", initialRating: 4 },
+  { name: "Git", initialRating: 4 },
+];
+
+export const TOOLS = [
+  "Visual Studio Code",
+  "Git",
+  "Chrome DevTools",
+  "Npm (Node Package Manager)",
+  "Heroku",
+  "Postman",
+];
---
+++ src/components/SocialMedia.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+function SocialMedia() {
+  return (
+    <ul className="home-about-social-links">
+      <li className="social-icons">
+        <a
+          href="https://github.com/rasedul99"
+          target="_blank"
+          rel="noreferrer"
+          className="icon-colour  home-social-icons"
+        >
+          <AiFillGithub className="social-icon" />
+        </a>
+      </li>
+      <li className="social-icons">
+        <a
+          href="https://www.linkedin.com/in/rasedul-islam-2a0776201/"
+          target="_blank"
+          rel="noreferrer"
+          className="icon-colour  home-social-icons"
+        >
+          <FaLinkedinIn className="social-icon" />
+        </a>
+      </li>
+
+      <li className="social-icons">
+        <a
+          href="mailto:rasedul2199@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          className="icon-colour home-social-icons"
+        >
+          <MdEmail className="social-icon" />
+        </a>
+      </li>
+    </ul>
+  );
+}
+export default SocialMedia;
